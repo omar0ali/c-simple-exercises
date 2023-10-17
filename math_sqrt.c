db2b7d816d8c5e4246d35f64b19caa31bd09d196
omar0ali <bajunaidomar@gmail.com>
2023-10-17 01:05:12 -0400

    added sqrt

--- math_sqrt.c
+++ math_sqrt.c
@@ -2,13 +2,29 @@
 #include<stdlib.h>
 #include "math_header.h" 
 
+int math_square(int num) {
+    return num * num;
+}
 
-double math_sqrt() {
-    //TODO: Implement sqrt function.
-    return 0.0;
+int math_sqrt(int find) {
+    int result = 0;
+    int squareNumber = 1;
+    while(1) {
+        result = math_square(squareNumber);
+        if(result >= find) {
+            break;
+        }
+        squareNumber++;
+    }
+    return squareNumber;
 }
 
 int main (int c, char** args) {
     //TODO: use the sqrt function.
+     if (c < 1) {
+        printf("Usage: %s <number>\n", args[0]);
+        return 1;
+    }
+    printf("Result: %d\n", (int) math_sqrt(atoi(args[1])));
     return 0;
 }
