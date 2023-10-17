5d3f5e65130501f3f8cd6a5ff4f751fe41823211
omar0ali <bajunaidomar@gmail.com>
2023-10-17 01:33:27 -0400

    added sqrt updated, using double and increasing accuracy.

--- math_sqrt.c
+++ math_sqrt.c
@@ -2,23 +2,22 @@
 #include<stdlib.h>
 #include "math_header.h" 
 
-int math_square(int num) {
+double math_square(double num) {
     return num * num;
 }
 /** This function is not 100% accurate, but conceptually a valid solution.
  * Lack of Precision: The method of iterating and squaring numbers is approximate. It won't give you an exact square root, which can be problematic for applications where precision matters.
  * Inefficient: This approach requires a loop that iterates through numbers, squaring each one until it meets the criteria. For large numbers, this can be very slow.
- * Limited to Integers: This method only works for integer square roots.
-*/
-int math_sqrt(int find) {
-    int result = 0;
-    int squareNumber = 1;
+ */
+double math_sqrt(double find) {
+    double result = 0.0;
+    double squareNumber = 1.0;
     while(1) {
         result = math_square(squareNumber); //Square the number
         if(result >= find) { //Check if its equal or larger than what we need to find.
             break;
         }
-        squareNumber++;
+        squareNumber+= 0.1;
     }
     return squareNumber;
 }
@@ -29,6 +28,6 @@ int main (int c, char** args) {
         printf("Usage: %s <number>\n", args[0]);
         return 1;
     }
-    printf("Result: %d\n", (int) math_sqrt(atoi(args[1])));
+    printf("Result: %8.3lf\n", (double) math_sqrt(atoi(args[1])));
     return 0;
 }
